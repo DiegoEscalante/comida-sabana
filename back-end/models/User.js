c31168c59f66702ea0051698e60b443fa90efa2b
Diego Escalante <descalanteg05@gmail.com>
2025-04-04 18:00:18 -0500

    Created a middleware that hashes passwords with bcrypt

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = new mongoose.Schema({
     id: {type: Number, unique: true, validate: {validator: Number.isInteger}, required: true},
@@ -8,6 +10,18 @@ const userSchema = new mongoose.Schema({
     lastName: {type: String, required: true},
     role: {type: Number, validate: {validator: Number.isInteger}, required: true},
     restaurantID: {type: Number, required: true}
+})
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next(); // In case the password hasn't changed, it doesn't hash it again.
+    try{
+        const hashedPassword = bcrypt.hash(this.password, saltRounds);
+        this.password = hashedPassword;
+        next();
+    } catch (error){
+        next(error); // Throws error up the chain in case the password couldn't be hashed
+    }
 })
 
 module.exports = mongoose.model('User', userSchema);
