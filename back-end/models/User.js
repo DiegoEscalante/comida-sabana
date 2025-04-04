546506c0b0bc9bd81fc4815c1a459d03207f3bfc
unknown <descalanteg05@gmail.com>
2025-04-04 14:27:25 -0500

    Added User schema

---
+++ back-end/models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    id: {type: Number, unique: true, validate: {validator: Number.isInteger}, required: true},
+    email: {type: String, unique: true},
+    name: {type: String, required: true},
+    lastName: {type: String, required: true},
+    role: {type: Number, validate: {validator: Number.isInteger}, required: true},
+    restaurantID: {type: Number, required: true}
+})
