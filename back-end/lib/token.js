06a43496c95dd8f54150bcb4b5bd8005662bc019
Diego Escalante <descalanteg05@gmail.com>
2025-04-11 17:36:32 -0500

    Fixed a typo that was saving the refresh token incorrectly

--- back-end/lib/token.js
+++ back-end/lib/token.js
@@ -12,7 +12,7 @@ const generateAccessToken = (user) => {
 
 const generateRefreshToken = (user) => {
     return jwt.sign(
-        {id: user},
+        {id: user.id},
         process.env.JWT_REFRESH_SECRET,
         {expiresIn:'30d'}
     )
