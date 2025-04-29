3f1a6af4e9a68762db5a6befb2304353f0bfe59c
unknown <descalanteg05@gmail.com>
2025-04-29 13:33:50 -0500

    Exported admin router

--- back-end/routes/adminRoutes.js
+++ back-end/routes/adminRoutes.js
@@ -5,3 +5,5 @@ const authorize = require('../middlewares/authorize');
 const adminController = require('../controllers/adminController');
 
 router.put('/users/:id/assign-pos', authenticate, authorize('admin'), adminController.assignPosToUser); // Only admin can assign pos to users
+
+module.exports = router;
