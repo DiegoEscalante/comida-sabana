414cea363e6cbc31f75dbf7d015f7ec0020d81ea
unknown <descalanteg05@gmail.com>
2025-04-28 21:38:24 -0500

    Fixed endpoints for products (removed duplicate routes)

--- back-end/routes/productRoutes.js
+++ back-end/routes/productRoutes.js
@@ -4,10 +4,10 @@ const authenticate = require('../middlewares/authenticate');
 const authorize = require('../middlewares/authorize');
 const router = express.Router();
 
-router.get('/products/', authenticate, productController.getAllProducts); 
-router.get('/products/:id', authenticate, productController.getProductById);
-router.post('/products/', authenticate, authorize('pos'), productController.createProduct); //Only pos can create products
-router.put('/products/:id', authenticate, authorize('pos'), productController.updateProduct); // Only pos can update products
-router.delete('/products/:id', authenticate, authorize('pos'), productController.deleteProduct); // Only pos can delete products
-router.get('/products/restaurant/:restaurantId', authenticate, productController.getProductsByRestaurantId); 
+router.get('/', authenticate, productController.getAllProducts); 
+router.get('/restaurant/:restaurantId', authenticate, productController.getProductsByRestaurantId); 
+router.get('/:id', authenticate, productController.getProductById);
+router.post('/', authenticate, authorize('pos'), productController.createProduct); //Only pos can create products
+router.put('/:id', authenticate, authorize('pos'), productController.updateProduct); // Only pos can update products
+router.delete('/:id', authenticate, authorize('pos'), productController.deleteProduct); // Only pos can delete products
 module.exports = router;
