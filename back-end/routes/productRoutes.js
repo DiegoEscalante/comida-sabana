3dac0d0f69e02116b21aeaaece59f075b3230a96
unknown <descalanteg05@gmail.com>
2025-04-28 22:20:38 -0500

    Created a product validation function

--- back-end/routes/productRoutes.js
+++ back-end/routes/productRoutes.js
@@ -2,12 +2,13 @@ const express = require('express');
 const productController = require('../controllers/productController');
 const authenticate = require('../middlewares/authenticate');
 const authorize = require('../middlewares/authorize');
+const validateProduct = require('../middlewares/validateProduct');
 const router = express.Router();
 
 router.get('/', authenticate, productController.getProducts); 
 router.get('/restaurant/:restaurantId', authenticate, productController.getProductsByRestaurantId); 
 router.get('/:id', authenticate, productController.getProductById);
-router.post('/', authenticate, authorize('pos'), productController.createProduct); //Only pos can create products
+router.post('/', authenticate, authorize('pos'), validateProduct, productController.createProduct); //Only pos can create products
 router.put('/:id', authenticate, authorize('pos'), productController.updateProduct); // Only pos can update products
 router.delete('/:id', authenticate, authorize('pos'), productController.deleteProduct); // Only pos can delete products
 module.exports = router;
