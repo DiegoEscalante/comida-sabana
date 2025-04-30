f35f93181d885a0e156dccd7aa2b25fed66a37a9
unknown <descalanteg05@gmail.com>
2025-04-30 12:26:22 -0500

    Authorize POS by product's restaurantId

--- back-end/routes/productRoutes.js
+++ back-end/routes/productRoutes.js
@@ -3,12 +3,13 @@ const productController = require('../controllers/productController');
 const authenticate = require('../middlewares/authenticate');
 const authorize = require('../middlewares/authorize');
 const validateProduct = require('../middlewares/validateProduct');
+const authorizePOSByProduct = require('../middlewares/authorizePOSbyProduct'); 
 const router = express.Router();
 
 router.get('/', authenticate, productController.getProducts); 
 router.get('/restaurant/:restaurantId', authenticate, productController.getProductsByRestaurantId); 
 router.get('/:id', authenticate, productController.getProductById);
 router.post('/', authenticate, authorize('pos'), validateProduct, productController.createProduct); //Only pos can create products
-router.put('/:id', authenticate, authorize('pos'), productController.updateProduct); // Only pos can update products
-router.delete('/:id', authenticate, authorize('pos'), productController.deleteProduct); // Only pos can delete products
+router.put('/:id', authenticate, authorize('pos'), authorizePOSByProduct, productController.updateProduct); // Only pos can update products
+router.delete('/:id', authenticate, authorize('pos'), authorizePOSByProduct, productController.deleteProduct); // Only pos can delete products
 module.exports = router;
