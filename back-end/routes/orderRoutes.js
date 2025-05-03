105c4da50391cb255fa53d64bb47367aaca02676
Diego Escalante <descalanteg05@gmail.com>
2025-05-03 10:00:36 -0500

    Check for POS access beforing accessing restaurant's orders

--- back-end/routes/orderRoutes.js
+++ back-end/routes/orderRoutes.js
@@ -4,12 +4,14 @@ const orderController = require('../controllers/orderController');
 const { orderValidationSchemaPOS, orderValidationSchemaClient, validateOrder } = require('../middlewares/validateOrder');
 const authorize = require('../middlewares/authorize');
 const authenticate = require('../middlewares/authenticate');
+const authorizePOSByRestaurant = require('../middlewares/authorizePOSbyRestaurant');
 
 
 router.post('/client', authenticate, authorize('client'), orderValidationSchemaClient, validateOrder, orderController.createOrderClient);
 router.post('/pos', authenticate, authorize('pos'), orderValidationSchemaPOS, validateOrder, orderController.createOrderPOS);
+router.get('/restaurant/:restaurantId', authenticate, authorize('pos'), authorizePOSByRestaurant, orderController.getOrdersByRestaurant);
 router.get('/', authenticate, orderController.getOrders);
-router.get('/restaurant/:restaurantId', orderController.getOrdersByRestaurant);
+
 router.patch('/:orderId', orderController.updateOrderStatus);
 router.put('/:orderId/status', authenticate, authorize('pos'), orderController.putOrderStatus);
 
