f1273ae68954091f8b6f13fa456c60911747de24
Diego Escalante <descalanteg05@gmail.com>
2025-05-03 11:40:52 -0500

    Fixed exported function

--- back-end/lib/calculateDeliveryTime.js
+++ back-end/lib/calculateDeliveryTime.js
@@ -1,7 +1,7 @@
 // Calcular el tiempo de entrega en minutos
-function calculateDeliveryTime(start, end) {
+const calculateDeliveryTime = (start, end) => {
     const ms = new Date(end) - new Date(start);
     return ms > 0 ? Math.floor(ms / 60000) : null;
-}
+};
 
 module.exports = calculateDeliveryTime;
