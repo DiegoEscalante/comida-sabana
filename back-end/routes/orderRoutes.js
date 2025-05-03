e3d8b490fa4933d96d69b39ab6d549274402e6f5
Diego Escalante <descalanteg05@gmail.com>
2025-05-03 08:37:35 -0500

    Used correct validation schemas for each user

--- back-end/routes/orderRoutes.js
+++ back-end/routes/orderRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orderController');
-const { orderValidationSchema, validateOrder } = require('../middlewares/validateOrder');
+const { orderValidationSchemaPOS, orderValidationSchemaClient, validateOrder } = require('../middlewares/validateOrder');
 const authorize = require('../middlewares/authorize');
 const authenticate = require('../middlewares/authenticate');
 
 
-router.post('/client', authenticate, authorize('client'), orderValidationSchema, validateOrder, orderController.createOrderClient);
-router.post('/pos', authenticate, authorize('pos'), orderValidationSchema, validateOrder, orderController.createOrderPOS);
+router.post('/client', authenticate, authorize('client'), orderValidationSchemaClient, validateOrder, orderController.createOrderClient);
+router.post('/pos', authenticate, authorize('pos'), orderValidationSchemaPOS, validateOrder, orderController.createOrderPOS);
 router.get('/', authenticate, orderController.getOrders);
 router.get('/restaurant/:restaurantId', orderController.getOrdersByRestaurant);
 router.patch('/:orderId', orderController.updateOrderStatus);
