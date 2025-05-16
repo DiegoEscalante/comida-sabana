cc36cf9c5fa0f9bccc655c19bd842826603c03b2
Diego Escalante <descalanteg05@gmail.com>
2025-05-16 17:45:40 -0500

    Exported restaurant controller

--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const authenticate = require('../middlewares/authenticate');
 const router = express.Router();
+const restaurantController = require('../controllers/restaurantController');
 
 router.get('/', authenticate, restaurantController.getRestaurants); 
 
