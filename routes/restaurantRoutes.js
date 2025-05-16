c7c6221e2ac3cd8f298d809edc66830a8d6d195e
Diego Escalante <descalanteg05@gmail.com>
2025-05-16 17:41:20 -0500

    Added an endpoint to get all restaurants

--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const authenticate = require('../middlewares/authenticate');
 const router = express.Router();
 
-
+router.get('/', authenticate, restaurantController.getRestaurants); 
 
 module.exports = router;
