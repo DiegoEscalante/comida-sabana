8ed9f334485a121c08ed2f4ae1275dc1586445ba
Diego Escalante <descalanteg05@gmail.com>
2025-05-16 19:46:50 -0500

    get only restaurant reviews with comments

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -88,8 +88,11 @@ const getRestaurantReviews = async (req, res) => {
             return res.status(400).json({ message: 'ID de restaurante inválido.' });
         }
 
-        const reviews = await Review.find({ restaurantId })
-            .populate('userId', 'name') 
+        const reviews = await Review.find({
+            restaurantId,
+            comment: { $exists: true, $ne: '' }
+        })
+            .populate('userId', 'name')
             .sort({ createdAt: -1 });
 
         res.status(200).json(reviews);
